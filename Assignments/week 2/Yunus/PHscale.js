cb9fa9d593f24b9addfce509ced10c81cf9a9cc8
yunushamod <yunushamod@gmail.com>
2020-10-18 08:14:37 +0100

    YunusHamd submitting assignment

---
+++ Assignments/week 2/Yunus/PHscale.js
@@ -0,0 +1,12 @@
+let PH;
+
+
+
+    if(PH <7 && PH > 0) console.log("Acidic");
+    else if(PH  >7 &&PH <=14) console.log("Alkaline");
+    else if(PH == 7) console.log("Neutral");
+    else{
+        console.log("Invalid");
+    }
+
+
